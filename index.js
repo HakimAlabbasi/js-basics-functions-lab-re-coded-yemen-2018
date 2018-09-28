da421f2a157d06def188079e32d0318e904a83e4
Anood <anoodtoo8@gmail.com>
2018-09-28 14:45:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+  return 43%43;
+}
+distanceFromHqInBlocks (43);
+distanceTravelledInFeet(34){
+  return 34*264;
+}
+distanceFromHqInBlocks(43);
